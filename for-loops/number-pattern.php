b7a88b8cfe06e541bab82f5be29d5a8e055d7107
Hoshiyarbanga <hoshiyarbanga@gmail.com>
2024-03-21 13:52:02 +0530

    Your commit message here

---
+++ for-loops/number-pattern.php
@@ -0,0 +1,23 @@
+<?php
+// #1
+$limit = 5;
+for ($i=1;$i<=$limit;$i++){
+$inc=$limit-1;
+$print = $i;
+echo $i;
+for($j=2;$j<=$i;$j++){
+echo $print =$print+$inc;
+$inc--;
+}
+echo '<br>';
+}
+
+// Output of #1
+// 1
+// 2 6 
+// 3 7 10
+// 4 8 11 13
+// 5 9 12 14 15
+
+
+#2
