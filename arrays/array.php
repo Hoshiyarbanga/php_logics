a3d61e68340efa5bfefc6d84973bd622774b6b3d
Hoshiyarbanga <hoshiyarbanga@gmail.com>
2024-03-26 10:50:43 +0530

    Your commit message here

--- arrays/array.php
+++ arrays/array.php
@@ -1,17 +1,44 @@
 <?php
-// let array1 = [{firstname:'hosi',id:1},
-//              {firstname:'ravi',id:2},
-//              {firstname:'kamal',id:3},
-//              {firstname:'priya',id:4},
-//              {firstname:'shivani',id:5}]
+#1
+/* let array1 = [{firstname:'hosi',id:1},
+             {firstname:'ravi',id:2},
+             {firstname:'kamal',id:3},
+             {firstname:'priya',id:4},
+             {firstname:'shivani',id:5}]
 
-// let selected = [1,3,5,2];
+let selected = [1,3,5,2];
 
 
-// let selectedArray = [];
-// for(let i=0; i<selected.length; i++){
-//     let name = array1.find(person => person.id === selected[i]);
-//     selectedArray.push(name.firstname);
-// }
+let selectedArray = [];
+for(let i=0; i<selected.length; i++){
+    let name = array1.find(person => person.id === selected[i]);
+    selectedArray.push(name.firstname);
+} */
  
-//  console.log(selectedArray); //['hoshi','kamal','shivani','ravi']
+// console.log(selectedArray); //['hoshi','kamal','shivani','ravi']
+
+#2
+/*$path = __DIR__ .'\abc.json';
+$jsonString = file_get_contents($path);
+$jsonData = json_decode($jsonString, true);
+$result = [];
+foreach ($jsonData as $key=> $value) {
+    if(isset($value["productPacks"]) && !empty(($value["productPacks"]))){
+    foreach ($value["productPacks"] as $key ) {
+    $abc = $key["productPackName"];
+    preg_match_all('/\((.*?)\)/i', $abc, $matches);
+    $a = str_replace("(","",$matches[0][1]);
+    $b = str_replace(")","",$a);
+    $c = explode(";", $b);
+    array_push($result,$c[0]);
+       }
+     }
+    }
+    echo "<pre>";
+    print_r($result);
+*/
+
+//output --
+
+#3
+   
--- for-loops/number-pattern.php
+++ for-loops/number-pattern.php
@@ -21,3 +21,13 @@
 
 
 #2
+
+// <?php
+// $string = 'DuoDERM CGF Dressing (Hydrocolloid Dressing) (15 cm x 15 cm; 187661) [5]';
+
+// preg_match_all('/\((.*?)\)/i', $string, $matches);
+// $a = str_replace("(","",$matches[0][1]);
+// $b = str_replace(")","",$a);
+// $c = explode(";", $b);
+// echo $c[0];
+// ?>
