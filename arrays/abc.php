2b754dc33d8db21fa8c8e98a85985453e61339b0
Hoshiyarbanga <hoshiyarbanga@gmail.com>
2024-04-18 09:58:04 +0530

    comit

--- arrays/abc.php
+++ arrays/abc.php
@@ -1,1 +1,25 @@
 
+<?php
+function covert($number){
+ $path = __DIR__ .'\abcd.json';
+$jsonString = file_get_contents($path);
+$jsonData = json_decode($jsonString, true);
+$result ='';
+start:
+$abc = array_keys($jsonData);
+rsort($abc);
+ foreach(  $abc as $val){
+    if( $number >= $val ){
+        foreach ($jsonData as $bal => $xyz ){
+            if($bal === $val ){
+                $result .= $xyz ;
+                $number -= $bal;
+                goto start;
+            }
+        }
+    }
+ }
+ return $result;
+}
+echo covert(57);
+
